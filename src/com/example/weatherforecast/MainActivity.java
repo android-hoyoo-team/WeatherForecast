a4a838d20c407db6fe1b589e4d898cd3cbbce323
xufei <xunminian@163.com>
2015-05-05 22:29:07 +0800

    1

--- src/com/example/weatherforecast/MainActivity.java
+++ src/com/example/weatherforecast/MainActivity.java
@@ -8,6 +8,9 @@ public class MainActivity extends DroidGap {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		super.loadUrl("file:///android_asset/www/index.html");
+		//super.loadUrl("file:///android_asset/www/index.html");
+		//super.loadUrl("http://www.phonegap100.com/doc/cordova_file_file.md.html#File");
+		//super.loadUrl(Config.getStartUrl());
+		super.loadUrl("file:///android_asset/www/weather.html");
 	}
 }
